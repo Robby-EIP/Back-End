75e73b65c66f177463afa43d6633d3f04656f079
Paul MARLIERE <paul.marliere@epitech.eu>
2021-12-06 16:50:16 +0100

    [ADD] backend code blocs start

--- main.py
+++ main.py
@@ -1,7 +1,104 @@
-from fastapi import FastAPI
+from typing import Optional
+
+from fastapi import FastAPI, Body, Request, File, UploadFile, Form
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.responses import aiofiles
+import os
+import time
+# import aiofiles
+
+testvar:int = 0
 
 app = FastAPI()
+
+origins = [
+    "http://127.0.0.1",
+    "https://127.0.0.1:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Optional[bool] = None
+
+class TestItem(BaseModel):
+    code: str
+
+def push_ino():
+    os.system('mv *.ino assets.ino')
+    os.system('./../ArduinoExecs/arduino-cli board attach arduino:avr:uno')
+    os.system('./../ArduinoExecs/arduino-cli compile . -e')
+    os.system('./../ArduinoExecs/avrdude -C./../ArduinoExecs/avrdude.conf -v -c arduino -p atmega328p -P net:172.20.10.5:80 -Uflash:w:./build/arduino.avr.uno/assets.ino.hex:i')
+    os.system('rm -rf build')
+    os.system('rm *.ino')
+    os.system('rm sketch.json')
+
+def push_hex():
+    os.system('mv *.hex assets.hex')
+    os.system('./../ArduinoExecs/avrdude -C./../ArduinoExecs/avrdude.conf -v -c arduino -p atmega328p -P net:172.20.10.5:80 -Uflash:w:./assets.hex:i')
+    os.system('rm *.hex')
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.post('/push/rawcode')
+def get_rawcode(test: TestItem):
+    f = open('./assets/code' + str(testvar) + '.txt', "w")
+    f.write(test.code)
+    return {'success': 'true'}
+
+@app.post('/push/executable')
+async def image(file: UploadFile = File(...)):
+    print(file)
+    os.chdir('./assets')
+    async with aiofiles.open(f"./{file.filename}", 'wb+') as out_file:
+        content = await file.read()  # async read
+        await out_file.write(content)  # async write
+    temp = file.filename.split('.')
+    if temp[len(temp) - 1] == 'hex':
+        push_hex()
+    elif temp[len(temp) - 1] == 'ino':
+        push_ino()
+    os.chdir('..')
+    return {"success": "true"}
+
+@app.post('/push/blocs')
+def get_blocs(test: TestItem):
+    os.chdir('./assets')
+    f = open('./code' + '.bloc', "w")
+    f.write(test.code)
+    cmds = test.code.split(',')
+    print(cmds)
+    f2 = open('../example/starter.ino', "r")
+    example = f2.read()
+    example += '\n\nvoid loop() {\n'
+    for i in cmds:
+        if i == 'forward':
+            example += 'forward()'
+        elif i == 'right':
+            example += 'right()'
+        elif i == 'left':
+            example += 'left()'
+        elif i == 'back':
+            example += 'back()'
+        elif i.find('delay') != -1:
+            example += i
+        example += ';\n'
+    example += '}'
+    f3 = open('./assets.ino', "w")
+    f3.write(example)
+    f3.close()
+    push_ino()
+    os.system('rm code.bloc')
+    os.chdir('..')
+    return {'success': 'true'}
